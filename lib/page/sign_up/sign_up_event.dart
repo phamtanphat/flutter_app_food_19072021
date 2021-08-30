84f4c0341374a3f98b0bbacb9975e0cd97fe6704
phamtanphat <phatdroid94@gmail.com>
2021-08-30 18:30:01 +0700

    event

---
+++ lib/page/sign_up/sign_up_event.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_app_food/base/base_event.dart';
+
+abstract class SignUpEventBase extends BaseEvent{
+
+}
+
+class SignUpEvent extends SignUpEventBase{
+  final String fullName;
+  final String email;
+  final String phone;
+  final String password;
+  final String address;
+
+  SignUpEvent({
+    required this.fullName,
+    required this.email,
+    required this.phone,
+    required this.password,
+    required this.address,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
